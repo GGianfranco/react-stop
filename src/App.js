02480476afa91300fd305a92298ab61d2bb07212
GGianfranco <77826002+GGianfranco@users.noreply.github.com>
2022-02-17 14:46:45 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [px, setPx] = useState(24);
+  const [hexCode, setHexCode] = useState("ff0000");
+
+  const doubleInSize = () => {
+    setPx(px * 2);
+  };
+  const changeColor = () => {
+    const randomHexCode = Math.floor(Math.random() * 16777215).toString(16);
+    setHexCode(randomHexCode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button
+        className="stop"
+        onClick={() => {
+          doubleInSize();
+          changeColor();
+        }}
+        style={{
+          height: px * 4 + "px",
+          width: px * 4 + "px",
+          fontSize: px + "px",
+          color: "#" + hexCode,
+          borderColor: "#" + hexCode,
+          borderWidth: Math.floor(px / 2) + "px",
+        }}
+      >
+        STOP
+      </button>
     </div>
   );
 }
